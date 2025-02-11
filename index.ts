bdb56e938d52149e14d31cf7558630b3947aa470
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2025-02-12 02:48:24 +0800

    assign a dynamic pizza id

--- index.ts
+++ index.ts
@@ -10,20 +10,22 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-];
-
-
 let cashInRegister = 100;
 let nextOrderId = 1;
+let nextPizzaID = 1;
+
 const orderHistory: Order[] = []
+
+const menu: Pizza[] = [
+    { id: nextPizzaID++, name: "Margherita", price: 8 },
+    { id: nextPizzaID++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaID++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaID++, name: "Veggie", price: 9 },
+];
 
 function addNewPizza(pizzaObj: Pizza): void{ //function addNewPizza(pizzaObj: Pizza): void
     // will not return anything added Void as a return type
+    pizzaObj.id = nextPizzaID++;
     menu.push(pizzaObj);
 }
 
@@ -66,9 +68,9 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
 
 }
 
-addNewPizza({ id: 4, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 5, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 6, name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: nextPizzaID, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: nextPizzaID, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: nextPizzaID, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch");
 completeOrder(1);
