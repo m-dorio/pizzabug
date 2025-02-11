3a234e2109a841b5f39a6eade64e52ca1e9645c7
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2025-02-12 01:17:25 +0800

    typeof checker in TS

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 type Pizza = {
+    id: number
     name: string
     price: number
 }
@@ -6,14 +7,14 @@ type Pizza = {
 type Order = {
     id: number
     pizza: Pizza
-    status: string
+    status: "ordered" | "completed"
 }
 
-const menu = [
-    { name: "Margherita", price: 8},
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+    { id:1, name: "Margherita", price: 8 },
+    { id:2, name: "Pepperoni", price: 10 },
+    { id:3, name: "Hawaiian", price: 10 },
+    { id:4, name: "Veggie", price: 9 },
 ];
 
 
@@ -32,7 +33,7 @@ function placeOrder(pizzaName: string) {
         return;
     }
     cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderHistory.push(newOrder);
     return newOrder;
 }
@@ -48,9 +49,18 @@ function completeOrder(orderId: number) {
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+function getPizzaDetail(identifier: string | number) {
+    // const identify = menu.find(item => item.id === identifier || item.name === identifier); 
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase());
+    } else {
+        return menu.find(pizza=> pizza.id === identifier)
+    }
+}
+
+addNewPizza({id: 4, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({id: 5, name: "BBQ Chicken", price: 12 })
+addNewPizza({id: 6, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch");
 completeOrder(1);
