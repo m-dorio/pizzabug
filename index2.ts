a3257f3f1b74b860f44fd26032ac8fe4b9736712
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2025-02-11 10:25:55 +0800

    added first batch of checkers

---
+++ index2.ts
@@ -0,0 +1,26 @@
+let myName: string = "Bob";
+// myName = 5;
+
+let numberOfWheels : number = 4;
+let isStudent: boolean = false;
+
+type Food = string;
+let favoriteFood: Food = "pizza";
+
+type Person = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+};
+
+let person: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
